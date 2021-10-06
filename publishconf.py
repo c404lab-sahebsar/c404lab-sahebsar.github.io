2538828a73ef9e32ec9d4db3adc1d3de717f35d2
Mehrshad Sahebsara <msahebsara@live.ca>
2021-10-06 09:17:41 -0600

    Add theme and config changes

--- publishconf.py
+++ publishconf.py
@@ -14,7 +14,7 @@ SITEURL = 'c404lab-sahebsar.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
